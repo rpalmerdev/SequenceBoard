56dbad997a35e55f4a9eb5293d4318f496285dfc
rpalmerdev <gobobpalmer@gmail.com>
2023-12-21 22:56:04 -0500

    added requirements.txt

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ from PySide6 import QtWidgets
 from user_interface import UserInterface
 import assets_rc
 
-
 #load the ui - at runtime
 app = QtWidgets.QApplication(sys.argv)
-
 window = UserInterface()
 window.show()
-
-
 
 app.exec()
