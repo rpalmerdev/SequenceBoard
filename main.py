1d1ee7643e0198195b7544d602c3e40120c37950
rpalmerdev <gobobpalmer@gmail.com>
2023-12-23 10:46:45 -0500

    reworked UI to have 6 channel strips in a grid layout

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PySide6 import QtWidgets
 from user_interface import UserInterface
-import assets_rc
+
 
 #load the ui - at runtime
 app = QtWidgets.QApplication(sys.argv)
--- user_interface.py
+++ user_interface.py
@@ -1,29 +1,23 @@
 from PySide6 import QtCore, QtWidgets, QtGui 
 from PySide6.QtUiTools import QUiLoader
 
-#testing buttons
-def play_clicked():
-        print("Play button clicked!")
-def stop_clicked():
-        print("Stop button clicked!")
-def record_clicked():
-        print("Record button clicked!")
+
 
 #set up a loader object
 loader = QUiLoader()
 
-#object to wrap UI
 class UserInterface(QtCore.QObject):
     def __init__(self):
         super().__init__()
-        app = QtWidgets.QApplication.instance()  # get the current QApplication instance
-        app.setStyle('Fusion')  # set the style to Fusion
+        # get the current QApplication instance
+        app = QtWidgets.QApplication.instance()
+        # set the style 
+        app.setStyle('Fusion') 
         self.ui = loader.load("main_window.ui", None)
-        self.ui.play_audio_button.clicked.connect(play_clicked)
-        self.ui.stop_audio_button.clicked.connect(stop_clicked)
-        self.ui.record_audio_button.clicked.connect(record_clicked)
-         # Set the window icon
+        # Set the window icon
         self.ui.setWindowIcon(QtGui.QIcon('assets/icon.png'))
+        
+
+
     def show(self):
         self.ui.show()
- 
